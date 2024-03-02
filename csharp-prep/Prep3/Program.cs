1ef46979ee6f90f0e9ea6b5cb85729ae8c2f8b4c
Igberry <114621185+Igberry@users.noreply.github.com>
2024-03-02 11:50:18 +0100

    Finished Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,60 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Generate a random magic number between 1 and 100
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101);
+
+        int guess;
+        int guessCount = 0;
+        bool playAgain = true;
+
+        while (playAgain)
+        {
+            Console.WriteLine("Welcome to the Guess My Number game!");
+            Console.WriteLine("I've picked a number between 1 and 100. Try to guess it!");
+
+            // Reset guess count for each game
+            guessCount = 0;
+
+            // Loop until the user guesses the magic number
+            do
+            {
+                // Ask the user for a guess
+                Console.Write("Enter your guess: ");
+                guess = Convert.ToInt32(Console.ReadLine());
+
+                // Check if the guess is higher, lower, or correct
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Too low! Guess higher.");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Too high! Guess lower.");
+                }
+                else
+                {
+                    Console.WriteLine($"Congratulations! You've guessed the magic number {magicNumber}!");
+                }
+
+                guessCount++;
+
+            } while (guess != magicNumber);
+
+            // Inform the user of the number of guesses made
+            Console.WriteLine($"It took you {guessCount} guesses.");
+
+            // Ask the user if they want to play again
+            Console.Write("Do you want to play again? (yes/no): ");
+            string playAgainResponse = Console.ReadLine().ToLower();
+
+            // Check if the user wants to play again
+            if (playAgainResponse != "yes")
+            {
+                playAgain = false;
+                Console.WriteLine("Thank you for playing Guess My Number!");
+            }
+        }
     }
 }
