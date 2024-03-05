19caf5bdeefbba6b45b5c0fe115408cecb88f092
Igberry <114621185+Igberry@users.noreply.github.com>
2024-03-05 12:02:09 +0100

    Finished Learning Activity Week 02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Create job instances
+        Job job1 = new Job("Microsoft", "Software Engineer", 2019, 2022);
+        Job job2 = new Job("Apple", "Manager", 2022, 2023);
+
+        // Display job details
+        job1.Display();
+        job2.Display();
+
+        // Create resume instance
+        Resume myResume = new Resume("Allison Rose");
+
+        // Add jobs to the resume
+        myResume.AddJob(job1);
+        myResume.AddJob(job2);
+
+        // Display resume details
+        myResume.Display();
     }
 }
